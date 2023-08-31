227ee29d6a702d8dea235674b32aa7166aeea02f
Leopold Seidenglanz <leopold.seidenglanz@elev.ga.ntig.se>
2023-08-31 10:36:27 +0200

    dytjrk7

---
+++ Hello/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.WriteLine("Vad heter du?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"hej {name} vad snygg de är idag!");
+
+Console.WriteLine("Vad är din favorit Sport?");
+string sport = Console.ReadLine();
+
+
+Console.WriteLine($"Nice, {sport} är en bra sport.");
+
+Console.WriteLine("Vad är ditt favorit djur?");
+string djur = Console.ReadLine();
+
+
+Console.WriteLine($"{djur} är coola djur");
+
+Console.ReadLine();
